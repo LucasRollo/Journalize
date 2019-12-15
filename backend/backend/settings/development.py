20d7c48305d73d5162c7c0922858debd4c502500
Lucas Rollo <42591619+LucasRollo@users.noreply.github.com>
2019-12-15 01:24:09 -0600

    Update development.py

--- backend/backend/settings/development.py
+++ backend/backend/settings/development.py
@@ -4,7 +4,7 @@ from backend.settings.base import *
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+1@-^5q$qt*wu(m%fl0owx-cdlp-1^knh&roo_@c3wg$(2@rm='
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
