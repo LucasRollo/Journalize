af813b6f08447b7a853f79f6c39bb33c0f0d1005
Lucas Rollo <charleslucasrollo@gmail.com>
2019-12-14 20:13:17 -0600

    add dockerized react frontend

---
+++ backend/emotion_dictionaries.py
@@ -0,0 +1,20 @@
+
+anger = ["hostility", "violence", "pain", "tension", "terror", "bitter", "violent", "threat", "abuses", "suffer",
+ "lethal", "threats", "abuse", "fired", "attack", "attacked", "danger", "revenge", "vengenance", "critics", "worse",
+ "satan", "devil", "kill", "kills", "riot", "massacre", "shooting", "beaten", "beat", "shoot", "bloody", "enemy",
+ "enemies", "destruction", "destructive", "rape", "raped", "rapes", "cruel", "wounds", "wounded", "injured", "rumor",
+ "beating", "accusations", "accusation", "fault", "riots", "protest", "protests", "tensions", "threats", "rumors", 
+ "trouble", "troubles", "worst", "anger", "angry", "hangry", "mad", "demand", "demands", "ignored", "ignore", "ill",
+ "ignores", "murder", "murdered", "murders", "sick", "illness", "persecution", "sin", "guilt", "sins", "guilty", "ruin", 
+  "guilts", "weakness", "weaknesses", "suffers", "suffered", "suffering", " weakened", "weakening", "annoyed", "annoy",
+  "annoys", "annoying", "irritated", "irritation", "irritating", "irritate", "rage", "raging", "rages", "raged", "shit",
+  "offend", "offends", "offended", "offending", "sullen", "uptight", "stress", "stressed", "stressing", "tension",
+  "impatient", "exasperated", "exasperate", "exasperating", "stupid", "dumb", "idiot", "idiotic", "fuck", "fucked",
+  "senseless", "useless", "retarded", "retard", "displeasure", "unjust", "insult", "insulting", "indignant", "resent",
+  "resentful", "resent", "resents", "resenting", "resented", "resentment", "antagonist", "antagonize", "antagonizing",
+  "antagonized", "antagonism", "hostile", "hostility", "cynical", "cynicism", "mistrust", "untrustworthy", "doubt",
+  "irate", "enraged", "enrage", "enrages", "enraging", "dumped", "dump", "trash", "trashy", "garbage", "trashed", 
+  "furious", "smash", "smashing", "destroy", "destroyed", "destroying", "destroys", "ruined", "outrage", "outrageous",
+  "outraged", "blame", "negative", "aggravated", "fume", "fumes", "fuming", "unhappy", "not happy", "rage-quit", "panic"]
+
+sadness = ["sadness"]
