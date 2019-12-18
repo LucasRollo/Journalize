3434fb335d8599ea5d6ed9f7677a82e5e4883849
Lucas Rollo <charleslucasrollo@gmail.com>
2019-12-17 21:10:38 -0600

    fix app.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <div>
         <label htmlFor='char-input'>How many characters does</label>
         <input id='char-input' type='text' />
-        <button onClick={handleSubmit}>have?</button>
+        <button>have?</button>
       </div>
 
       <div>
         <h3 id='char-count'></h3>
       </div>
-=======
-      <Login />
->>>>>>> a469a77c6c33ef52588f5cb9d688a4b06b661a3e
     </div>
   );
 }
